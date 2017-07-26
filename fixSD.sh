ee60e787e4b9270297bf1ee1f375b75534e857cc
peter.pan <peter.pan@dorry.io>
2017-07-26 11:13:23 +0800

    add judgement of parameter,add usage and example

--- fixSD.sh
+++ fixSD.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+RED='\033[0;31m'
+NC='\033[0m'
+
+lsblk
+if [ $# -ne 1 ];then
+	echo ""
+	echo -e "${RED}input which disk u want to umount and fix${NC}"
+	echo -e "${RED}Usage:	sudo $0 DISK${NC}"
+	echo -e "${RED}Example: sudo $0 sdc2${NC}"
+	exit 1
+fi
 sudo umount /dev/$1
 lsblk
 sudo fsck.ext4 /dev/$1
